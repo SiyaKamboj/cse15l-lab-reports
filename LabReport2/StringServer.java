5794abf5ab068dfb7181c70ad3addeb9f5c99eb7
SiyaKamboj <siyakamboj20@gmail.com>
2023-01-30 17:41:33 -0800

    Update StringServer.java

--- LabReport2/StringServer.java
+++ LabReport2/StringServer.java
@@ -5,10 +5,12 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-
+    ArrayList<String> inputtedWords = new ArrayList<String>();
+    
+    /*
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Siya's Number: %d", num);
+            return ("Hello. Please append /add-message to the end of the URL");
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
@@ -23,6 +25,30 @@ class Handler implements URLHandler {
             }
             return "404 Not Found!";
         }
+    }
+    */
+    
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return ("Hello. Please append /add-message to the end of the URL");
+        } else if (url.getPath().equals("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                  inputtedWords[length] = parameters[1];
+                  return String.format(inputtedWords.toString());
+        }
+            /*
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
+            }
+            return "404 Not Found!";
+        }*/
     }
 }
 
